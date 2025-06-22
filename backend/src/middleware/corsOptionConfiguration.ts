0ce78490a17e89d0ddfd37924fb22b51111bcf9f
Arshad Rahim <rahimarshad667@gmail.com>
2025-06-22 21:30:15 +0530

    fix: cors problem

--- backend/src/middleware/corsOptionConfiguration.ts
+++ backend/src/middleware/corsOptionConfiguration.ts
@@ -1,6 +1,6 @@
 export const corsOptions = {
   origin:
-    process.env.CORS_ALLOWED_ORIGIN ||
+    // process.env.CORS_ALLOWED_ORIGIN ||
     "https://url-shortner-lemon-nine.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
