a2c1e085e4dcdc73e43186f4e953531c1cfda2d4
Arshad Rahim <rahimarshad667@gmail.com>
2025-06-25 10:31:08 +0530

    fix:setAuth cookies strict to none

--- backend/src/utils/cookieHelper.ts
+++ backend/src/utils/cookieHelper.ts
@@ -1,3 +1,27 @@
+// import { Response } from "express";
+
+// export const setAuthCookies = (
+//   res: Response,
+//   accessToken: string,
+//   refreshToken: string
+// ) => {
+//   const isProduction = process.env.NODE_ENV === "production";
+
+//   res.cookie("accessToken", accessToken, {
+//     httpOnly: true,
+//     secure: isProduction,
+//     sameSite: isProduction ? "strict" : "lax",
+//     maxAge: 3 * 60 * 60 * 1000, // 3 hours
+//   });
+
+//   res.cookie("refreshToken", refreshToken, {
+//     httpOnly: true,
+//     secure: isProduction,
+//     sameSite: isProduction ? "strict" : "lax",
+//     maxAge: 24 * 60 * 60 * 1000, // 24 hours
+//   });
+// };
+
 import { Response } from "express";
 
 export const setAuthCookies = (
@@ -10,14 +34,16 @@ export const setAuthCookies = (
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction ? "strict" : "lax",
+    sameSite: isProduction ? "none" : "lax",
+    path: "/",
     maxAge: 3 * 60 * 60 * 1000, // 3 hours
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction ? "strict" : "lax",
+    sameSite: isProduction ? "none" : "lax",
+    path: "/",
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
   });
 };
